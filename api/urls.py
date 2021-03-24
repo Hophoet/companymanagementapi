b7e04a162a8b4093c1aed248a3e21188d33edbe8
hophoet <hophoet@gmail.com>
2021-03-24 16:36:55 +0000

    (urls) - set AddNewTaskView view url

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('employees', views.GetEmployeesView.as_view(), name='get_employees'),
     path('employees/add', views.AddNewEmployeeView.as_view(), name='add_employee'),
     path('employees/update', views.UpdateEmployeeView.as_view(), name='update_employee'),
-    path('employees/delete', views.DeleteEmployeeView.as_view(), name='delete_employee')
+    path('employees/delete', views.DeleteEmployeeView.as_view(), name='delete_employee'),
+
+    path('tasks/add', views.AddNewTaskView.as_view(), name='add_task')
 ]
