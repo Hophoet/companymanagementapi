24d6a1193aec8e79730f784995ee9a27b84ab08c
hophoet <hophoet@gmail.com>
2021-03-24 11:23:57 +0000

    (serializers) - add UpdateEmployeeSerializer for the employee endpoint view data validation checking

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,10 @@ class AddNewEmployeeSerializer(serializers.Serializer):
     salary = serializers.IntegerField()
     picture = serializers.ImageField() 
 
+
+class UpdateEmployeeSerializer(serializers.Serializer):
+    """ update employee end point serializer """
+    employee_id = serializers.IntegerField()
+    username = serializers.CharField()
+    salary = serializers.IntegerField()
+    picture = serializers.ImageField() 
