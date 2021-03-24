fc0d42a75829b4d976133d38bddd152562cba3a0
hophoet <hophoet@gmail.com>
2021-03-24 12:06:32 +0000

    (urls) - set DeleteEmployeeView url

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ app_name = "api"
 
 urlpatterns = [
     path('employees/add', views.AddNewEmployeeView.as_view(), name='add_employee'),
-    path('employees/update', views.UpdateEmployeeView.as_view(), name='update_employee')
+    path('employees/update', views.UpdateEmployeeView.as_view(), name='update_employee'),
+    path('employees/delete', views.DeleteEmployeeView.as_view(), name='delete_employee')
 ]
