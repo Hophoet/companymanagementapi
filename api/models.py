61709b5dd713cf0d53d77a0d3fa3a70e9d50acb2
hophoet <hophoet@gmail.com>
2021-03-24 10:09:36 +0000

    (models) - add api main models, User, Profil, Task

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# User model, this model represent the employee and the admins
+User = get_user_model()
+
+
+# Employee task, represent a task that can be assigned to the employee by the admin
+class Task(models.Model):
+    """ Employee tasks model """
+    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='employee')
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    dealine = models.DateTimeField()
+
+
+# Profil model represent the additinal information for the employee
+class Profil(models.Model):
+    """ Employee Profile, for additional informations for the user """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField()
+    salary = models.IntegerField()
