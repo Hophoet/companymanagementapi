abe281910350f1eaed3aedab59cc593fe83db246
hophoet <hophoet@gmail.com>
2021-03-24 12:06:14 +0000

    (views) - add DeleteEmployeeView for the employees deletion management

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.utils import IntegrityError
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
-from .serializers import AddNewEmployeeSerializer, UpdateEmployeeSerializer
+from .serializers import AddNewEmployeeSerializer, UpdateEmployeeSerializer, DeleteEmployeeSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_200_OK
-from .models import User, Profil
+from .models import Profil
 
 
 #
@@ -71,5 +72,32 @@ class UpdateEmployeeView(APIView):
         profil.salary = salary
         profil.picture = picture
         profil.save()
-
         return Response(data=serializer.data, status=HTTP_200_OK)
+
+class DeleteEmployeeView(APIView):
+    """ employee deleting management by admin view """
+    permission_classes = (IsAdminUser,)
+    serializer_class = DeleteEmployeeSerializer
+
+    def delete(self, request, *args, **kwargs):
+        """ post request method """
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request':request}
+        )
+        serializer.is_valid(raise_exception=True)
+        #
+        employee_id = serializer.data.get('employee_id')
+        try:
+            employee = User.objects.get(id=employee_id)
+        except ObjectDoesNotExist as error:
+            return Response(data={'text':'employee not exists'}, status=HTTP_400_BAD_REQUEST)
+         
+        try:
+            Profil.objects.get(user=employee)
+        except ObjectDoesNotExist as error:
+            return Response(data={'text':f'this user{employee} it not an employee'}, status=HTTP_400_BAD_REQUEST)
+
+        employee.delete()
+
+        return Response(data={'text':f'employee({employee}) deleted successfully'}, status=HTTP_200_OK)
