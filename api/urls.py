31c2350b26b700e214fc9dde955e575641529e32
hophoet <hophoet@gmail.com>
2021-03-24 11:24:58 +0000

    (urls) - set the UpdateEmployeeView url

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "api"
 
 urlpatterns = [
-    path('employees/add', views.AddNewEmployeeView.as_view(), name='add_employee')
+    path('employees/add', views.AddNewEmployeeView.as_view(), name='add_employee'),
+    path('employees/update', views.UpdateEmployeeView.as_view(), name='update_employee')
 ]
