09e21aee6c1976cc1a29e8b3c3937a060f52e27f
hophoet <hophoet@gmail.com>
2021-03-24 16:06:09 +0000

    (urls) - set GetEmployeesView view url

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "api"
 
 urlpatterns = [
+    path('employees', views.GetEmployeesView.as_view(), name='get_employees'),
     path('employees/add', views.AddNewEmployeeView.as_view(), name='add_employee'),
     path('employees/update', views.UpdateEmployeeView.as_view(), name='update_employee'),
     path('employees/delete', views.DeleteEmployeeView.as_view(), name='delete_employee')
