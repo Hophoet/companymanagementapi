84287ba68a25b61093dedd048b28fa09d7237ac7
hophoet <hophoet@gmail.com>
2021-03-27 23:03:22 +0000

    (tests) - add test_get_employees to test the employees getting request

--- api/tests.py
+++ api/tests.py
@@ -39,6 +39,7 @@ class EmployeeTestCase(APITestCase):
 
 
     def generate_photo_file(self):
+        """ image file generator """
         file = io.BytesIO()
         image = Image.new('RGBA', size=(100, 100), color=(155, 0, 0))
         image.save(file, 'png')
@@ -64,3 +65,17 @@ class EmployeeTestCase(APITestCase):
         self.assertEqual(response.status_code, HTTP_200_OK)
 
 
+    def test_get_employees(self):
+        """ test the employees avaialble getting 
+        (request) -> 200 """
+        user = User.objects.create(is_staff=True, username='test user', password='jkld')
+
+        employees_before_employee_created = User.objects.filter(is_staff=False).count()
+        #create new employee
+        User.objects.create(username='test employee', password='password')
+
+        self.api_client.force_authenticate(user=user)
+        response = self.api_client.get(self.get_employees_url)
+        employees_after_user_created = User.objects.filter(is_staff=False).count()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(employees_after_user_created, employees_before_employee_created + 1)
