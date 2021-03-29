341de6c35b311d4b8a4ac66eae65458140d5caf6
hophoet <hophoet@gmail.com>
2021-03-29 10:08:00 +0000

    (urls) - update urls

--- companymanagementapi/urls.py
+++ companymanagementapi/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('e00435de1ed4/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration', include('rest_auth.registration.urls')),
+    path('auth/', include('rest_auth.urls')),
+    path('register', include('rest_auth.registration.urls')),
     path('api/', include('api.urls'))
 ]
 
