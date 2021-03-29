72061a421211865e66d0deeb32e29816c214fd79
hophoet <hophoet@gmail.com>
2021-03-29 09:35:33 +0000

    (settings) - update SECRET_KEY, set as an env variable

--- companymanagementapi/settings.py
+++ companymanagementapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8yo)d)z+o$=3rr0o^-g3zlwc!n7h$q9cw=-oa-8a&3o#^-8b9k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
